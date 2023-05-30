336db02db6f5ad91ca37c64743141506d1aefe7c
zoerix <zoe.rix.z@gmail.com>
2023-05-30 14:05:26 +0200

    added files

---
+++ promise-loader-ex/index.js
@@ -0,0 +1,29 @@
+
+const loaderContainerElement = document.querySelector('.loaderContainer');
+console.log(loaderContainerElement);
+
+const loaderContainer = document.querySelector('.lds-heart');
+console.log(loaderContainer);
+
+const pageElement = document.getElementById('page');
+console.log(pageElement);
+
+
+pageElement.style.display = "none";
+
+function waitForPage(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve (true);
+        }, time);
+    });
+}
+
+waitForPage(6000).then(
+    (val) => { 
+        if(val) {
+            pageElement.style.display = "block";    
+            loaderContainer.style.display = "none";
+        }
+     }
+)
